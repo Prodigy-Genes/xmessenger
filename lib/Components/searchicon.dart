cf14ee7597d02442ac2b527b50a8c035d8150887
Prodigy-Genes <bulletoze@gmail.com>
2023-10-22 15:01:33 +0000

    trying to implement the search function

--- lib/Components/searchicon.dart
+++ lib/Components/searchicon.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SearchIcon extends SearchDelegate<String> {
+
+  final List<String> contacts;
+  final List<String> message;
+
+  SearchIcon(this.contacts, this.message);
   @override
 
   List<Widget> buildActions (BuildContext context){
@@ -24,22 +29,36 @@ class SearchIcon extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context){
+    final results = contacts.where((item) => item.contains(query)).toList();
+
     return Container(
       color: const Color.fromARGB(255, 44, 0, 62), 
-      child: Center(
-        child: Text('Search results for: $query', 
+      child: ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(results[index], 
         style: const TextStyle(color: Color.fromARGB(255, 0, 255, 145))),
+          );
+        } 
       ),
     );
   }
 
   @override
   Widget buildSuggestions(BuildContext context){
+    final suggestions = message.where((item) => item.contains(query)).toList();
+
     return Container(
       color: const Color.fromARGB(255, 44, 0, 62), 
-      child: Center(
-        child: Text('Suggestion results for: $query', 
+      child: ListView.builder(
+        itemCount: suggestions.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(suggestions[index], 
         style: const TextStyle(color: Color.fromARGB(255, 0, 255, 145))),
+          );
+        } 
       ),
     );
 
